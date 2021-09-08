198771d675d9411cd0f062e6bcb3c69ac4a9a55c
Sandaruwan <isjsandaruwan@gmail.com>
2021-09-08 22:58:44 +0530

    add a spinner to the project

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import axios from "axios";
 import AllNav from "./components/AllNav";
 import React, { useEffect, useState } from "react";
 
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 axios.defaults.withCredentials = true;
 
 function App() {
-
+  // Sprinner
   const [isLoading, setLoading] = useState(true);
   function fakeRequest() {
-    return new Promise(resolve => setTimeout(() => resolve(), 800));
+    return new Promise((resolve) => setTimeout(() => resolve(), 800));
   }
 
   useEffect(() => {
@@ -27,15 +27,11 @@ function App() {
     return null;
   }
 
-
   return (
-    
     <div>
       <Router>
         <AllNav />
       </Router>
-
-
     </div>
   );
 }
