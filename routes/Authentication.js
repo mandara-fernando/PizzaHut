21781bcc5f418a5ee416ddb6b7c82ba271fb13c0
Sandaruwan Jayasundara <68706530+Sandaruwan-Jayasundara@users.noreply.github.com>
2021-09-10 08:00:25 -0700

    update Authentication Routes

--- routes/Authentication.js
+++ routes/Authentication.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+// Authentication Routes
 const Authentication = require("../api/Authentication/Auth.api");
 router.use("/auth-signup", Authentication);
 
